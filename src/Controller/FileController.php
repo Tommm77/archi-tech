39362e591c1bda32a1f5f58a70a77d2d6e36b22f
Tommm77 <picout.tom@gmail.com>
2023-09-09 03:21:25 +0200

    Implement Upload logic for File

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\File;
 
 class FileController extends AbstractController
 {
@@ -19,5 +23,42 @@ class FileController extends AbstractController
             'isAdmin' => $isAdmin,
         ]);
     }
+
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    #[Route('/upload', name: 'file_upload', methods: ['POST', 'GET'])]
+    public function upload(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $request->files->get('file');
+            if ($uploadedFile) {
+                // Stockez uniquement les informations sans déplacer le fichier
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // Créez et stockez l'entité File
+                $file = new File();
+                $file->setFilename($originalFilename);
+                $file->setFilesize($uploadedFile->getSize());
+                $file->setFiletype($uploadedFile->getMimeType());
+                $file->setDescription($request->request->get('description'));
+                $file->setUploadDate(new \DateTime());
+                $file->setCreateDate(new \DateTime());
+                $file->setOwner($this->getUser());
+                $file->setFilepath("uploads/test/");
+
+                $this->entityManager->persist($file);
+                $this->entityManager->flush();
+
+                $this->addFlash('success', 'File details saved successfully!');
+                return $this->redirectToRoute('my_files');
+            }
+        }
+
+        return $this->render('file/upload.html.twig');
+    }
+
 
 }
