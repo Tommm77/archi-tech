70630ae721206c93bf1419c19767e2c6db6be1b1
Tommm77 <picout.tom@gmail.com>
2023-09-09 03:30:59 +0200

    User can Delete his file

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\File;
+use App\Entity\User;
 
 class FileController extends AbstractController
 {
@@ -35,10 +36,8 @@ class FileController extends AbstractController
             /** @var UploadedFile $uploadedFile */
             $uploadedFile = $request->files->get('file');
             if ($uploadedFile) {
-                // Stockez uniquement les informations sans déplacer le fichier
                 $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
 
-                // Créez et stockez l'entité File
                 $file = new File();
                 $file->setFilename($originalFilename);
                 $file->setFilesize($uploadedFile->getSize());
@@ -59,6 +58,27 @@ class FileController extends AbstractController
 
         return $this->render('file/upload.html.twig');
     }
+
+    #[Route('/file/{id}/delete', name: 'file_delete', methods: ['GET'])]
+public function delete(int $id): Response
+{
+    $file = $this->entityManager->getRepository(File::class)->find($id);
+    if (!$file) {
+        throw $this->createNotFoundException('The file does not exist.');
+    }
+
+    // Vérifiez si l'utilisateur actuel est le propriétaire du fichier ou s'il est admin
+    if ($file->getOwner() === $this->getUser() || $this->isGranted('ROLE_ADMIN')) {
+        $this->entityManager->remove($file);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'File deleted successfully!');
+    } else {
+        $this->addFlash('error', 'You do not have permission to delete this file.');
+    }
+
+    return $this->redirectToRoute('my_files');
+}
 
 
 }
--- src/Entity/File.php
+++ src/Entity/File.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\FileRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\User;
 
 #[ORM\Entity(repositoryClass: FileRepository::class)]
 class File
@@ -16,7 +17,7 @@ class File
 
     #[ORM\ManyToOne(inversedBy: 'files')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?user $owner = null;
+    private ?User $owner = null;
 
     #[ORM\Column(length: 255)]
     private ?string $filename = null;
